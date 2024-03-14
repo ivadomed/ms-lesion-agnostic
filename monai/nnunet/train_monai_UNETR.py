56479550e6a58df73d9a5946281dbf963e321cd3
Pierre-Louis Benveniste <pierrelouis.benveniste03@gmail.com>
2024-03-14 18:44:39 -0400

    working monai script based on Jan's code: but no dice score improvement

--- monai/nnunet/train_monai_UNETR.py
+++ monai/nnunet/train_monai_UNETR.py
@@ -22,6 +22,14 @@ import monai
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import yaml
+import numpy as np
+import wandb
+import time
+from loguru import logger
+
+from monai.networks.layers import Norm
+
+os.environ["PYTORCH_USE_CUDA_DSA"] = "1"
 
 
 #Transforms import
@@ -40,7 +48,8 @@ from monai.transforms import (
     RandAdjustContrastd,
     AsDiscreted,
     RandHistogramShiftd,
-    ResizeWithPadOrCropd
+    ResizeWithPadOrCropd,
+    EnsureTyped
     )
 
 # Dataset import
@@ -49,7 +58,7 @@ from monai.data import DataLoader, CacheDataset, load_decathlon_datalist, Datase
 
 # model import
 import torch
-from monai.networks.nets import UNETR
+from monai.networks.nets import UNETR, UNet
 from monai.losses import DiceLoss
 
 # For training and validation
@@ -70,60 +79,80 @@ def get_parser():
     return parser
 
 
-def validation(model, epoch_iterator_val, config, post_label, post_pred, dice_metric, global_step):
-    model.eval()
-    with torch.no_grad():
-        for batch in epoch_iterator_val:
-            val_inputs, val_labels = (batch["image"].cuda(), batch["label"].cuda())
-            val_outputs = sliding_window_inference(val_inputs, config["spatial_size"], 4, model)
-            val_labels_list = decollate_batch(val_labels)
-            val_labels_convert = [post_label(val_label_tensor) for val_label_tensor in val_labels_list]
-            val_outputs_list = decollate_batch(val_outputs)
-            val_output_convert = [post_pred(val_pred_tensor) for val_pred_tensor in val_outputs_list]
-            dice_metric(y_pred=val_output_convert, y=val_labels_convert)
-            epoch_iterator_val.set_description("Validate (%d / %d Steps)" % (global_step, 10.0))  # noqa: B038
-        mean_dice_val = dice_metric.aggregate().item()
-        dice_metric.reset()
-    return mean_dice_val
+# def validation(model, epoch_iterator_val, config, post_label, post_pred, dice_metric, global_step):
+#     model.eval()
+#     with torch.no_grad():
+#         for batch in epoch_iterator_val:
+#             val_inputs, val_labels = (batch["image"].cuda(), batch["label"].cuda())
+#             val_outputs = model(val_inputs)
+#             dice_metric(y_pred=val_outputs, y=val_labels)
+#             # val_outputs = sliding_window_inference(val_inputs, config["spatial_size"], 1, model)
+#             # val_labels_list = decollate_batch(val_labels)
+#             # val_labels_convert = [post_label(val_label_tensor) for val_label_tensor in val_labels_list]
+#             # val_outputs_list = decollate_batch(val_outputs)
+#             # val_output_convert = [post_pred(val_pred_tensor) for val_pred_tensor in val_outputs_list]
+#             # dice_metric(y_pred=val_output_convert, y=val_labels_convert)
+#             epoch_iterator_val.set_description("Validate (%d / %d Steps)" % (global_step, 10.0))  # noqa: B038
+#         # for batch in epoch_iterator_val:
+#         #         val_inputs, val_labels = (
+#         #             batch["image"].cuda(),
+#         #             batch["label"].cuda(),
+#         #         )
+#         #         # TODO: parametrize this
+#         #         roi_size = config["spatial_size"]
+#         #         sw_batch_size = 4
+#         #         val_outputs = sliding_window_inference(
+#         #             val_inputs, roi_size, sw_batch_size, model)
 
-def train(model, config, global_step, train_loader, dice_val_best, global_step_best, loss_function, optimizer, epoch_loss_values, metric_values, val_loader, post_label, post_pred, dice_metric):
-    model.train()
-    epoch_loss = 0
-    step = 0
-    epoch_iterator = tqdm(train_loader, desc="Training (X / X Steps) (loss=X.X)", dynamic_ncols=True)
-    for step, batch in enumerate(epoch_iterator):
-        step += 1
-        x, y = (batch["image"].cuda(), batch["label"].cuda())
-        logit_map = model(x)
-        loss = loss_function(logit_map, y)
-        loss.backward()
-        epoch_loss += loss.item()
-        optimizer.step()
-        optimizer.zero_grad()
-        epoch_iterator.set_description(  # noqa: B038
-            "Training (%d / %d Steps) (loss=%2.5f)" % (global_step, config["max_iterations"], loss)
-        )
-        if (global_step % config["eval_num"] == 0 and global_step != 0) or global_step == config["max_iterations"]:
-            epoch_iterator_val = tqdm(val_loader, desc="Validate (X / X Steps) (dice=X.X)", dynamic_ncols=True)
-            dice_val = validation(model, epoch_iterator_val, config, post_label, post_pred, dice_metric, global_step)
-            epoch_loss /= step
-            epoch_loss_values.append(epoch_loss)
-            metric_values.append(dice_val)
-            if dice_val > dice_val_best:
-                dice_val_best = dice_val
-                global_step_best = global_step
-                torch.save(model.state_dict(), config["best_model_path"])
-                print(
-                    "Model Was Saved ! Current Best Avg. Dice: {} Current Avg. Dice: {}".format(dice_val_best, dice_val)
-                )
-            else:
-                print(
-                    "Model Was Not Saved ! Current Best Avg. Dice: {} Current Avg. Dice: {}".format(
-                        dice_val_best, dice_val
-                    )
-                )
-        global_step += 1
-    return global_step, dice_val_best, global_step_best
+#         #         val_outputs = [post_pred(i) for i in decollate_batch(val_outputs)]
+#         #         val_labels = [post_label(i) for i in decollate_batch(val_labels)]
+#         #         # compute metric for current iteration
+#         #         dice_metric(y_pred=val_outputs, y=val_labels)
+            
+#         mean_dice_val = dice_metric.aggregate().item()
+#         print("Mean dice val: ", mean_dice_val)
+#         dice_metric.reset()
+
+#     return mean_dice_val
+
+# def train(model, config, global_step, train_loader, dice_val_best, global_step_best, loss_function, optimizer, epoch_loss_values, metric_values, val_loader, post_label, post_pred, dice_metric):
+#     model.train()
+#     epoch_loss = 0
+#     step = 0
+#     epoch_iterator = tqdm(train_loader, desc="Training (X / X Steps) (loss=X.X)", dynamic_ncols=True)
+#     for step, batch in enumerate(epoch_iterator):
+#         step += 1
+#         x, y = (batch["image"].cuda(), batch["label"].cuda())
+#         logit_map = model(x)
+#         loss = loss_function(logit_map, y)
+#         loss.backward()
+#         epoch_loss += loss.item()
+#         optimizer.step()
+#         optimizer.zero_grad()
+#         epoch_iterator.set_description(  # noqa: B038
+#             "Training (%d / %d Steps) (loss=%2.5f)" % (global_step, config["max_iterations"], loss)
+#         )
+#         if (global_step % config["eval_num"] == 0 and global_step != 0) or global_step == config["max_iterations"]:
+#             epoch_iterator_val = tqdm(val_loader, desc="Validate (X / X Steps) (dice=X.X)", dynamic_ncols=True)
+#             dice_val = validation(model, epoch_iterator_val, config, post_label, post_pred, dice_metric, global_step)
+#             epoch_loss /= step
+#             epoch_loss_values.append(epoch_loss)
+#             metric_values.append(dice_val)
+#             if dice_val > dice_val_best:
+#                 dice_val_best = dice_val
+#                 global_step_best = global_step
+#                 torch.save(model.state_dict(), config["best_model_path"])
+#                 print(
+#                     "Model Was Saved ! Current Best Avg. Dice: {} Current Avg. Dice: {}".format(dice_val_best, dice_val)
+#                 )
+#             else:
+#                 print(
+#                     "Model Was Not Saved ! Current Best Avg. Dice: {} Current Avg. Dice: {}".format(
+#                         dice_val_best, dice_val
+#                     )
+#                 )
+#         global_step += 1
+#     return global_step, dice_val_best, global_step_best
 
 
 def main():
@@ -194,6 +223,7 @@ def main():
                 nonzero=False, 
                 channel_wise=False
             ),
+            EnsureTyped(keys=["image", "label"]),
             AsDiscreted(
                 keys=["label"],
                 num_classes=2,
@@ -228,6 +258,7 @@ def main():
                 nonzero=False, 
                 channel_wise=False
             ),
+            EnsureTyped(keys=["image", "label"]),
             AsDiscreted(
                 keys=["label"],
                 num_classes=2,
@@ -251,23 +282,21 @@ def main():
         os.makedirs(output_dir)
 
     # We load the train and validation data
-    print("Loading the training and validation data...")
-    # train_files = load_decathlon_datalist(data, True, "train")
+    logger.info("Loading the training and validation data...")
     train_ds = CacheDataset(
                             data=train_list,
                             transform=train_transforms,
-                            cache_rate=0.25,
-                            num_workers=4
+                            cache_rate=0.1,
+                            num_workers=2
                             )
-    train_loader = DataLoader(train_ds, batch_size=1, shuffle=True, num_workers=8, pin_memory=True, persistent_workers=True)
-    # val_files = load_decathlon_datalist(data, True, "validation")
+    train_loader = DataLoader(train_ds, batch_size=config["batch_size"], shuffle=True, num_workers=4, pin_memory=True, persistent_workers=True)
     val_ds = CacheDataset(
                         data=val_list,
                         transform=val_transforms,
-                        cache_rate=0.25,
-                        num_workers=4,
+                        cache_rate=0.1,
+                        num_workers=0,
                         )
-    val_loader = DataLoader(val_ds, batch_size=1, shuffle=False, num_workers=8, pin_memory=True)  
+    val_loader = DataLoader(val_ds, batch_size=1, shuffle=False, num_workers=0, pin_memory=True)  
 
     # plot 3 image and save them
     fig, axes = plt.subplots(1, 3, figsize=(15, 5))
@@ -279,43 +308,146 @@ def main():
     plt.savefig(os.path.join(output_dir, "image.png"))
 
 
-    print("Preparing the UNETR model...")
-    # we define the device to use
-    device = torch.device("cuda")
 
-    model = UNETR(
+    print("Preparing the UNET model...")
+    # we define the device to use
+    device = torch.device("cuda:0")
+
+    model = UNet(
+        spatial_dims=3,
         in_channels=1,
         out_channels=1,
-        img_size=config["spatial_size"],
-        feature_size=config["feature_size"],
-        hidden_size=config["hidden_size"],
-        mlp_dim=config["mlp_dim"],
-        num_heads=config["num_heads"],
-        proj_type="perceptron",
-        norm_name="instance",
-        res_block=True,
-        dropout_rate=0.0,
+        channels=(16, 32, 64, 128, 256),
+        strides=(2, 2, 2, 2),
+        kernel_size=3,
+        up_kernel_size=3,
+        num_res_units=0,
+        act='PRELU',
+        norm=Norm.BATCH,
+        dropout=0.0,
+        bias=True,
+        adn_ordering='NDA',
     ).to(device)
 
-    loss_function = DiceLoss(softmax=True)
-    torch.backends.cudnn.benchmark = True
+    loss_function = DiceLoss(to_onehot_y=True, softmax=True)
     optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-5)
+    dice_metric = DiceMetric(include_background=False, reduction="mean")
+    torch.backends.cudnn.benchmark = True
 
-    # We then train the model
-    post_label = AsDiscrete(to_onehot=1)
-    post_pred = AsDiscrete(argmax=True, to_onehot=1)
-    dice_metric = DiceMetric(include_background=True, reduction="mean", get_not_nans=False)
-    global_step = 0
-    dice_val_best = 0.0
-    global_step_best = 0
+    # initialize wandb
+    wandb.init(project=config["experiment_name"], config=config)
+
+    # 🐝 Log gen gradients of the models to wandb
+    wandb.watch(model, log_freq=100)
+
+    # 🐝 Add training script as an artifact
+    artifact_script = wandb.Artifact(name='training', type='file')
+    artifact_script.add_file(local_path=os.path.abspath(__file__), name=os.path.basename(__file__))
+    wandb.log_artifact(artifact_script)
+
     epoch_loss_values = []
-    metric_values = []
-    while global_step < config["max_iterations"]:
-        global_step, dice_val_best, global_step_best = train(model, config, global_step, train_loader, dice_val_best, global_step_best,
-                                                              loss_function, optimizer, epoch_loss_values, metric_values, val_loader, post_label, post_pred, dice_metric)
-    model.load_state_dict(torch.load(config["best_model_path"]))
+    step_loss_values = []
+    val_loss_values = []
+    best_val_loss = 1000.0
 
-    print(f"train completed, best_metric: {dice_val_best:.4f} " f"at iteration: {global_step_best}")
+    for epoch in range(config["max_iterations"]):
+        logger.info("-" * 10)
+        logger.info(f"epoch {epoch + 1}/{config['max_iterations']}")
+        model.train()
+        epoch_loss = 0
+        epoch_cl_loss = 0
+        epoch_recon_loss = 0
+        step = 0
+
+        for batch_data in train_loader:
+            step += 1
+            start_time = time.time()
+
+            inputs, gt_input = (
+                batch_data["image"].to(device),
+                batch_data["label"].to(device),
+            )
+        
+            optimizer.zero_grad()
+            output = model(inputs)
+
+            loss = loss_function(output, gt_input)
+            loss.detach().cpu()
+
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss.item()
+            step_loss_values.append(loss.item())
+
+
+            end_time = time.time()
+            logger.info(
+                f"{step}/{len(train_list) // train_loader.batch_size}, "
+                f"train_loss: {loss.item():.4f}, "
+                f"time taken: {end_time-start_time}s"
+            )
+
+            wandb.log({"Training/loss": loss.item()})
+
+        epoch_loss /= step
+
+        epoch_loss_values.append(epoch_loss)
+        
+        # 🐝 log train_loss averaged over epoch to wandb
+        wandb.log({"Training/loss_epoch": epoch_loss})
+
+
+        
+        logger.info(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
+
+        if epoch % config["eval_num"] == 0:
+            logger.info("Entering Validation for epoch: {}".format(epoch + 1))
+            total_val_loss = 0
+            val_step = 0
+            model.eval()
+            for val_batch in val_loader:
+                val_step += 1
+                start_time = time.time()
+                inputs, gt_input = (
+                    val_batch["image"].to(device),
+                    val_batch["label"].to(device),
+                )
+                outputs = model(inputs)
+                val_loss = loss_function(outputs, gt_input)
+                total_val_loss += val_loss.item()
+                end_time = time.time()
+
+            total_val_loss /= val_step
+            val_loss_values.append(total_val_loss)
+
+            wandb.log({"Validation loss": total_val_loss})
+
+            logger.info(f"epoch {epoch + 1} Validation avg loss: {total_val_loss:.4f}, " f"time taken: {end_time-start_time}s")
+
+            if total_val_loss < best_val_loss:
+                logger.info(f"Saving new model based on validation loss {total_val_loss:.4f}")
+                best_val_loss = total_val_loss
+                checkpoint = {"epoch": config["max_iterations"], "state_dict": model.state_dict(), "optimizer": optimizer.state_dict()}
+                torch.save(checkpoint, config["best_model_path"])
+
+    print("Done")
+
+
+    # # We then train the model
+    # post_label = AsDiscrete(to_onehot=2)
+    # post_pred = AsDiscrete(argmax=True, to_onehot=2)
+    # dice_metric = DiceMetric(include_background=False, reduction="mean", get_not_nans=False)
+    # global_step = 0
+    # dice_val_best = 0.0
+    # global_step_best = 0
+    # epoch_loss_values = []
+    # metric_values = []
+    # while global_step < config["max_iterations"]:
+    #     global_step, dice_val_best, global_step_best = train(model, config, global_step, train_loader, dice_val_best, global_step_best,
+    #                                                           loss_function, optimizer, epoch_loss_values, metric_values, val_loader, post_label, post_pred, dice_metric)
+    # model.load_state_dict(torch.load(config["best_model_path"]))
+
+    # print(f"train completed, best_metric: {dice_val_best:.4f} " f"at iteration: {global_step_best}")
 
 
 if __name__ == "__main__":
