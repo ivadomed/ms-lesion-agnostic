a42e91d8cff1c8011f26d70fcd925bca77ecd73a
Pierre-Louis Benveniste <pierrelouis.benveniste03@gmail.com>
2024-10-18 18:23:51 -0400

    added beijing data and orientation computation for all

--- dataset_analysis/msd_data_analysis.py
+++ dataset_analysis/msd_data_analysis.py
@@ -23,7 +23,7 @@ import nibabel as nib
 import numpy as np
 from pathlib import Path
 from image import Image
-
+from tqdm import tqdm
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -78,7 +78,7 @@ def main():
     # Now we will look at the average resolution of the images
     ## Iterate over the images
     resolutions = []
-    for image in data:
+    for image in tqdm(data):
         image_reoriented = Image(image['image']).change_orientation('RPI')
         resolution = image_reoriented.dim[4:7]
         resolution = [float(res) for res in resolution]
@@ -87,6 +87,8 @@ def main():
     print("Average resolution: ", np.mean(resolutions, axis=0))
     print("Std resolution: ", np.std(resolutions, axis=0))
     print("Median resolution: ", np.median(resolutions, axis=0))
+    print("Minimum pixel dimension: ", np.min(resolutions))
+    print("Maximum pixel dimension: ", np.max(resolutions))
 
     print("-------------------------------------")
 
@@ -119,6 +121,40 @@ def main():
         contrast_count_ms_basel_2020[contrast] = list_contrast_ms_basel_2020.count(contrast)
     print("Number of images per contrast in ms-basel-2020: ", contrast_count_ms_basel_2020)
 
+    # Now we will look at the average resolution of the images
+    basel_data = ms_basel_2018 + ms_basel_2020
+    ## Iterate over the images
+    resolutions_basel = []
+    orientation_basel = []
+    for image in tqdm(basel_data):
+        image_reoriented = Image(str(image)).change_orientation('RPI')
+        resolution = image_reoriented.dim[4:7]
+        resolution = [float(res) for res in resolution]
+        resolutions_basel.append(resolution)
+        if np.allclose(resolution, resolution[0], atol=1e-3):
+            orientation = 'iso'
+        elif np.argmax(resolution) == 0:
+            orientation = 'sag'
+        # Elif, the lowest arg is 1 then the orientation is coronal
+        elif np.argmax(resolution) == 1:
+            orientation = 'cor'
+        # Else the orientation is axial
+        else:
+            orientation = 'ax'
+        orientation_basel.append(orientation)
+        
+    print("Average resolution: ", np.mean(resolutions_basel, axis=0))
+    print("Std resolution: ", np.std(resolutions_basel, axis=0))
+    print("Median resolution: ", np.median(resolutions_basel, axis=0))
+    print("Minimum pixel dimension: ", np.min(resolutions_basel))
+    print("Maximum pixel dimension: ", np.max(resolutions_basel))
+
+    # Count the number of images per orientation
+    orientation_count_basel = {}
+    for orientation in set(orientation_basel):
+        orientation_count_basel[orientation] = orientation_basel.count(orientation)
+    print("Number of images per orientation in basel: ", orientation_count_basel)
+
     print("-------------------------------------")
     #############################################################
     # Now we can look at the external testing dataset umass*
@@ -139,6 +175,13 @@ def main():
     # Remove files containing ''SHA256E' in the name
     umass = [image for image in umass if 'SHA256E' not in str(image)]
 
+    # Remove : sub-ms1115_ses-01_acq-ax_ce-gad_T1w.nii.gz
+    umass = [image for image in umass if 'sub-ms1115_ses-01_acq-ax_ce-gad_T1w' not in str(image)]
+    # Remove: sub-ms1098_ses-01_acq-ax_ce-gad_T1w.nii.gz
+    umass = [image for image in umass if 'sub-ms1098_ses-01_acq-ax_ce-gad_T1w' not in str(image)]
+    # Remove: sub-ms1234_ses-03_acq-ax_ce-gad_T1w.nii.gz
+    umass = [image for image in umass if 'sub-ms1234_ses-03_acq-ax_ce-gad_T1w' not in str(image)]
+
     list_contrast_umass = [str(image).split("/")[-1].split('_')[-1].split('.')[0] for image in umass]
 
     print("Number of images in umass: ", len(umass))
@@ -148,6 +191,102 @@ def main():
     for contrast in set(list_contrast_umass):
         contrast_count_umass[contrast] = list_contrast_umass.count(contrast)
     print("Number of images per contrast in umass: ", contrast_count_umass)
+
+    # Now we will look at the average resolution of the images
+    ## Iterate over the images
+    resolutions_umass = []
+    orientation_umass = []
+    for image in tqdm(umass):
+        image_reoriented = Image(str(image)).change_orientation('RPI')
+        resolution = image_reoriented.dim[4:7]
+        resolution = [float(res) for res in resolution]
+        resolutions_umass.append(resolution)
+        if np.allclose(resolution, resolution[0], atol=1e-3):
+            orientation = 'iso'
+        elif np.argmax(resolution) == 0:
+            orientation = 'sag'
+        # Elif, the lowest arg is 1 then the orientation is coronal
+        elif np.argmax(resolution) == 1:
+            orientation = 'cor'
+            print(image)
+        # Else the orientation is axial
+        else:
+            orientation = 'ax'
+        orientation_umass.append(orientation)
+        
+    print("Average resolution: ", np.mean(resolutions_umass, axis=0))
+    print("Std resolution: ", np.std(resolutions_umass, axis=0))
+    print("Median resolution: ", np.median(resolutions_umass, axis=0))
+    print("Minimum pixel dimension: ", np.min(resolutions_umass))
+    print("Maximum pixel dimension: ", np.max(resolutions_umass))
+
+    # Count the number of images per orientation
+    orientation_count_umass = {}
+    for orientation in set(orientation_umass):
+        orientation_count_umass[orientation] = orientation_umass.count(orientation)
+    print("Number of images per orientation in umass: ", orientation_count_umass)
+
+    print("-------------------------------------")
+
+    #############################################################
+    # Now we can look at the external testing dataset ms-nmo-beijing
+    path_beijing = os.path.join(dataset_path, 'ms-nmo-beijing')
+    
+    beijing = list(Path(path_beijing).rglob("*/anat/*.nii.gz"))
+
+    # Keep only T1w images
+    beijing = [image for image in beijing if 'T1w' in str(image)]
+    # Remove Localizer images
+    beijing = [image for image in beijing if 'Localizer' not in str(image)]
+    beijing = [image for image in beijing if 'localizer' not in str(image)]
+
+
+
+    list_contrast_beijing = [str(image).split("/")[-1].split('_')[-1].split('.')[0] for image in beijing]
+
+    print("Number of images in beijing: ", len(beijing))
+    print("Contrast in beijing: ", set(list_contrast_beijing))
+    # Print the number of each contrast
+    contrast_count_beijing = {}
+    for contrast in set(list_contrast_beijing):
+        contrast_count_beijing[contrast] = list_contrast_beijing.count(contrast)
+    print("Number of images per contrast in beijing: ", contrast_count_beijing)
+
+    # Now we will look at the average resolution of the images
+    ## Iterate over the images
+    resolutions_beijing = []
+    orientations_beijing = []
+    for image in tqdm(beijing):
+        image_reoriented = Image(str(image)).change_orientation('RPI')
+        resolution = image_reoriented.dim[4:7]
+        resolution = [float(res) for res in resolution]
+        resolutions_beijing.append(resolution)
+        if np.allclose(resolution, resolution[0], atol=1e-3):
+            orientation = 'iso'
+        elif np.argmax(resolution) == 0:
+            orientation = 'sag'
+        # Elif, the lowest arg is 1 then the orientation is coronal
+        elif np.argmax(resolution) == 1:
+            orientation = 'cor'
+            print(image)
+        # Else the orientation is axial
+        else:
+            orientation = 'ax'
+        orientations_beijing.append(orientation)
+
+        
+    print("Average resolution: ", np.mean(resolutions_beijing, axis=0))
+    print("Std resolution: ", np.std(resolutions_beijing, axis=0))
+    print("Median resolution: ", np.median(resolutions_beijing, axis=0))
+    print("Minimum pixel dimension: ", np.min(resolutions_beijing))
+    print("Maximum pixel dimension: ", np.max(resolutions_beijing))
+
+    # Count the number of images per orientation
+    orientation_count_beijing = {}
+    for orientation in set(orientations_beijing):
+        orientation_count_beijing[orientation] = orientations_beijing.count(orientation)
+    print("Number of images per orientation in beijing: ", orientation_count_beijing)
+
 
     return None
 
