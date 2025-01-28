126957cba7b691e7660827acc62e60fae66e07c8
Pierre-Louis Benveniste <pierrelouis.benveniste03@gmail.com>
2025-01-28 16:18:00 -0500

    first version of training script

---
+++ meanTeacher/train.py
@@ -0,0 +1,207 @@
+"""
+This script trains a model to segment MS lesion from MRI images.
+The training is done using a Mean Teacher model, which is a semi-supervised learning method based on consistency training.
+
+Args:
+    --msd-data: path to the MSD dataset
+    --output-dir: path to the output directory
+
+Returns:
+    None
+
+Example:
+    $ python meanTeacher/train.py --msd-data data/MSD --output-dir output
+
+Author: Pierre-Louis Benveniste
+"""
+
+
+import os
+import torch
+import argparse
+from loguru import logger
+from monai.data import load_decathlon_datalist, CacheDataset, DataLoader
+from monai.transforms import Compose, LoadImaged, EnsureChannelFirstd, Orientationd, Spacingd, NormalizeIntensityd, RandCropByPosNegLabeld, ResizeWithPadOrCropd, RandFlipd, Rand3DElasticd, RandAffined, RandGaussianNoised, RandSimulateLowResolutiond, RandBiasFieldd
+from monai.networks.nets import AttentionUnet
+from monai.losses import DiceCELoss
+from tqdm import tqdm
+import torch.nn.functional as F
+from monai.inferers import sliding_window_inference
+
+
+def arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--msd-data', type=str, default='data/MSD', help='path to the MSD dataset')
+    parser.add_argument('--output-dir', type=str, default='output', help='path to the output directory')
+    return parser.parse_args()
+
+def train_transforms(data):
+    train_transforms = Compose(
+        [
+            LoadImaged(keys=["image", "label"], reader="NibabelReader"),
+            EnsureChannelFirstd(keys=["image", "label"]),
+            Orientationd(keys=["image", "label"], axcodes="RPI"),
+            Spacingd(keys=["image", "label"],pixdim=[0.7, 0.7, 0.7],mode=(2, 0)),
+            ResizeWithPadOrCropd(keys=["image", "label"],spatial_size=[64, 128, 128]),
+            NormalizeIntensityd(keys=["image"], nonzero=False, channel_wise=False),   
+        ]
+    )
+    return train_transforms(data)
+
+def val_transforms(data):
+    val_transforms = Compose(
+        [
+            LoadImaged(keys=["image", "label"], reader="NibabelReader"),
+            EnsureChannelFirstd(keys=["image", "label"]),
+            Orientationd(keys=["image", "label"], axcodes="RPI"),
+            Spacingd(keys=["image", "label"],pixdim=[0.7, 0.7, 0.7],mode=(2, 0)),
+            ResizeWithPadOrCropd(keys=["image", "label"],spatial_size=[64, 128, 128]),
+            NormalizeIntensityd(keys=["image"], nonzero=False, channel_wise=False),   
+        ]
+    )
+    return val_transforms(data)
+
+def main():
+    # Parse the arguments
+    args = arg_parser()
+    msd_data = args.msd_data
+    output_dir = args.output_dir
+
+    # Device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Load the data
+    logger.info(f'Loading data from {msd_data}')
+    train_files = load_decathlon_datalist(msd_data, True, "train")
+    val_files = load_decathlon_datalist(msd_data, True, "validation")
+    test_files = load_decathlon_datalist(msd_data, True, "test")
+
+    train_cache_rate = 0.5
+    val_cache_rate = 0.25
+    train_ds = CacheDataset(data=train_files, transform=train_transforms, cache_rate=train_cache_rate, num_workers=8)
+    val_ds = CacheDataset(data=val_files, transform=val_transforms, cache_rate=val_cache_rate, num_workers=8)
+
+    # Create the dataloaders
+    train_loader = DataLoader(train_ds, batch_size=2, shuffle=True, num_workers=8, pin_memory=True, persistent_workers=True)
+    val_loader = DataLoader(val_ds, batch_size=1, shuffle=False, num_workers=0, pin_memory=True, persistent_workers=False)
+
+    # Create the models
+    studentNet = AttentionUnet(
+            spatial_dims=3,
+            in_channels=1,
+            out_channels=1,
+            channels=[32, 64, 128, 256, 512],
+            strides=[2, 2, 2, 2, 2],
+            dropout=0.1,
+    ).to(device)
+    TeacherNet = AttentionUnet(
+            spatial_dims=3,
+            in_channels=1,
+            out_channels=1,
+            channels=[32, 64, 128, 256, 512],
+            strides=[2, 2, 2, 2, 2],
+            dropout=0.1,
+    ).to(device)
+
+    # Create the optimizer
+    optimizer = torch.optim.AdamW(studentNet.parameters(), lr=0.0001, weight_decay=0.00001)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=250)
+
+    # Create the loss function
+    supervised_loss = DiceCELoss(sigmoid=False, smooth_dr=1e-4)
+    consistency_loss = F.cross_entropy()
+
+    # Initialize the storage of training results
+    train_sup_losses = []
+    train_cons_losses = []
+    train_total_losses = []
+    val_sup_losses = []
+    val_cons_losses = []
+    val_total_losses = []
+
+    # Train the model
+    logger.info('Training the model')
+    studentNet.train()
+    TeacherNet.train()
+    epoch_loss = 0
+    step = 0
+    epoch_iterator = tqdm(train_loader, desc="Training (X / X Steps) (loss=X.X)", dynamic_ncols=True)
+
+    for step, batch in enumerate(epoch_iterator):
+        step += 1
+        inputs, labels = (batch["image"].cuda(), batch["label"].cuda())
+
+        # ------------ For the student model ------------
+        logit_map_student = studentNet(inputs)
+        # Get probabilities from logits
+        output_student = F.relu(logit_map_student) / F.relu(logit_map_student).max() if bool(F.relu(logit_map_student).max()) else F.relu(logit_map_student)
+        # Compute the supervised loss
+        supervised_loss = supervised_loss(output_student, labels)
+        train_sup_losses.append(supervised_loss.item())
+
+        # ------------ For the teacher model ------------
+        logit_map_teacher = TeacherNet(inputs)
+        # Get probabilities from logits
+        output_teacher = F.relu(logit_map_teacher) / F.relu(logit_map_teacher).max() if bool(F.relu(logit_map_teacher).max()) else F.relu(logit_map_teacher)
+        # Compute the consistency loss
+        consistency_loss = consistency_loss(output_student, output_teacher)
+        train_cons_losses.append(consistency_loss.item())
+
+        # Compute the total loss
+        loss = 0.5*(supervised_loss + consistency_loss)
+        train_total_losses.append(loss.item())
+
+        # Backward pass
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+        optimizer.zero_grad()
+
+        # Update the Teacher model weights
+        # TODO: update the teacher model weights: refer to this: https://github.com/perone/mean-teacher/blob/79bf9fc61f540491b79d3b3e60a213f798c3ea27/pytorch/main.py#L182
+
+        # Report the losses in the logger
+        logger.info(f'Step {step}/{len(train_loader)}, Supervised loss: {supervised_loss.item()}, Consistency loss: {consistency_loss.item()}, Total loss: {loss.item()}')
+
+        # Evaluate the model
+        if step % 10 == 0:
+            studentNet.eval()
+            TeacherNet.eval()
+            with torch.no_grad():
+                inputs, labels = batch["image"], batch["label"]
+
+                # With the student model
+                # NOTE: this calculates the loss on the entire image after sliding window
+                logits_student = sliding_window_inference(inputs, [64, 128, 128], mode="gaussian",
+                                                sw_batch_size=4, predictor=studentNet, overlap=0.5,) 
+                # get probabilities from logits
+                outputs_student = F.relu(logits_student) / F.relu(logits_student).max() if bool(F.relu(logits_student).max()) else F.relu(logits_student)
+                # calculate supervised validation loss
+                val_sup_loss = supervised_loss(outputs_student, labels)
+                val_sup_losses.append(val_sup_loss.item())
+
+                # With the teacher model
+                logits_teacher = sliding_window_inference(inputs, [64, 128, 128], mode="gaussian",
+                                                sw_batch_size=4, predictor=TeacherNet, overlap=0.5,)
+                # get probabilities from logits
+                outputs_teacher = F.relu(logits_teacher) / F.relu(logits_teacher).max() if bool(F.relu(logits_teacher).max()) else F.relu(logits_teacher)
+                # calculate consistency validation loss
+                val_cons_loss = consistency_loss(outputs_student, outputs_teacher)
+                val_cons_losses.append(val_cons_loss.item())
+
+                # calculate total validation loss
+                val_total_loss = 0.5*(val_sup_loss + val_cons_loss)
+                val_total_losses.append(val_total_loss.item())
+
+                # Report the validation losses in the logger
+                logger.info(f'Validation losses: Supervised loss: {val_sup_loss.item()}, Consistency loss: {val_cons_loss.item()}, Total loss: {val_total_loss.item()}')
+
+                # If the total loss is the best, report the performances and save the teacher model
+                if val_total_loss == min(val_total_losses):
+                    logger.info(f'Best model found at step {step}')
+                    # torch.save(TeacherNet.state_dict(), os.path.join(output_dir, 'best_model.pth'))
+
+
+if __name__ == '__main__':
+    main()
