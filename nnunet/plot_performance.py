2c61231b1f91f17360fd9c1ca7195cb30efa3485
Pierre-Louis Benveniste <pierrelouis.benveniste03@gmail.com>
2025-01-28 14:23:07 -0500

    added saving of mean values of results

--- nnunet/plot_performance.py
+++ nnunet/plot_performance.py
@@ -68,7 +68,7 @@ def main():
                 # Add the contrat, the site and the resolution to the df
                 test_dice_results.loc[test_dice_results['name'] == file, 'contrast'] = data['contrast']
                 test_dice_results.loc[test_dice_results['name'] == file, 'site'] = data['site']
-                test_dice_results.loc[test_dice_results['name'] == file, 'orientation'] = data['orientation']
+                # test_dice_results.loc[test_dice_results['name'] == file, 'acquisition'] = data['acquisition']
                 test_dice_results.loc[test_dice_results['name'] == file, 'nb_lesions'] = data['nb_lesions']
                 test_dice_results.loc[test_dice_results['name'] == file, 'total_lesion_volume'] = data['total_lesion_volume']
 
@@ -83,8 +83,8 @@ def main():
     test_dice_results['site_count'] = test_dice_results['site'].apply(lambda x: x + f' (n={site_counts[x]})')
 
     # Same for the resolution
-    resolution_counts = test_dice_results['orientation'].value_counts()
-    test_dice_results['orientation_count'] = test_dice_results['orientation'].apply(lambda x: x + f' (n={resolution_counts[x]})')
+    # resolution_counts = test_dice_results['acquisition'].value_counts()
+    # test_dice_results['acquisition_count'] = test_dice_results['acquisition'].apply(lambda x: x + f' (n={resolution_counts[x]})')
 
     # then we add the ppv score to the df
     ppv_score_file = path_to_outputs + '/ppv_scores.txt'
@@ -173,55 +173,53 @@ def main():
     # Print the mean dice score per contrast and std
     print("\nDice score per contrast (mean ± std)")
     dice_stats = metrics_results.groupby('contrast_count')['dice_score'].agg(['mean', 'std'])
+    # print global dice score and std
+    print(f"Global dice score: {metrics_results['dice_score'].mean():.4f} ± {metrics_results['dice_score'].std():.4f}")
     for contrast, row in dice_stats.iterrows():
         print(f"{contrast}: {row['mean']:.4f} ± {row['std']:.4f}")
 
     # Print the mean ppv score per contrast and std
     print("\nPPV score per contrast (mean ± std)")
     ppv_stats = metrics_results.groupby('contrast_count')['ppv_score'].agg(['mean', 'std'])
+    # print global ppv score and std
+    print(f"Global ppv score: {metrics_results['ppv_score'].mean():.4f} ± {metrics_results['ppv_score'].std():.4f}")
     for contrast, row in ppv_stats.iterrows():
         print(f"{contrast}: {row['mean']:.4f} ± {row['std']:.4f}")
     
     # Print the mean f1 score per contrast and std
     print("\nF1 score per contrast (mean ± std)")
     f1_stats = metrics_results.groupby('contrast_count')['f1_score'].agg(['mean', 'std'])
+    # print global f1 score and std
+    print(f"Global f1 score: {metrics_results['f1_score'].mean():.4f} ± {metrics_results['f1_score'].std():.4f}")
     for contrast, row in f1_stats.iterrows():
         print(f"{contrast}: {row['mean']:.4f} ± {row['std']:.4f}")
 
     # Print the mean sensitivity score per contrast and std
     print("\nSensitivity score per contrast (mean ± std)")
     sensitivity_stats = metrics_results.groupby('contrast_count')['sensitivity_score'].agg(['mean', 'std'])
+    # print global sensitivity score and std
+    print(f"Global sensitivity score: {metrics_results['sensitivity_score'].mean():.4f} ± {metrics_results['sensitivity_score'].std():.4f}")
     for contrast, row in sensitivity_stats.iterrows():
         print(f"{contrast}: {row['mean']:.4f} ± {row['std']:.4f}")
-
-    # # plot a violin plot per site
-    # plt.figure(figsize=(20, 10))
-    # plt.grid(True)
-    # sns.violinplot(x='site_count', y='dice_score', data=test_dice_results, order = ['bavaria-quebec (n=208)', 'sct-testing-large (n=233)', 'canproco (n=71)','nih (n=25)','basel (n=32)'])
-    # # y ranges from -0.2 to 1.2
-    # plt.ylim(-0.2, 1.2)
-    # plt.title('Dice scores per site')
-    # plt.show()
-
-    # # Save the plot
-    # plt.savefig(path_to_outputs + '/dice_scores_site.png')
-    # print(f"Saved the dice_scores per site plot in {path_to_outputs}")
-
-    # # plot a violin plot per resolution
-    # plt.figure(figsize=(20, 10))
-    # plt.grid(True)
-    # sns.violinplot(x='orientation_count', y='dice_score', data=test_dice_results, order = ['iso (n=58)', 'ax (n=343)', 'sag (n=168)'])
-    # # y ranges from -0.2 to 1.2
-    # plt.ylim(-0.2, 1.2)
-    # plt.title('Dice scores per orientation')
-    # plt.show()
-
-    # # Save the plot
-    # plt.savefig(path_to_outputs + '/dice_scores_orientation.png')
-    # print(f"Saved the dice_scores per orientation plot in {path_to_outputs}")
-
-    # # Save the test_dice_results dataframe
-    # test_dice_results.to_csv(path_to_outputs + '/dice_results.csv', index=False)
+    
+    # Save the prints in a txt file
+    with open(path_to_outputs + '/metrics_stats.txt', 'w') as f:
+        f.write("Dice score per contrast (mean ± std)\n")
+        f.write(f"Global dice score: {metrics_results['dice_score'].mean():.4f} ± {metrics_results['dice_score'].std():.4f}\n")
+        for contrast, row in dice_stats.iterrows():
+            f.write(f"{contrast}: {row['mean']:.4f} ± {row['std']:.4f}\n")
+        f.write("\nPPV score per contrast (mean ± std)\n")
+        f.write(f"Global ppv score: {metrics_results['ppv_score'].mean():.4f} ± {metrics_results['ppv_score'].std():.4f}\n")
+        for contrast, row in ppv_stats.iterrows():
+            f.write(f"{contrast}: {row['mean']:.4f} ± {row['std']:.4f}\n")
+        f.write("\nF1 score per contrast (mean ± std)\n")
+        f.write(f"Global f1 score: {metrics_results['f1_score'].mean():.4f} ± {metrics_results['f1_score'].std():.4f}\n")
+        for contrast, row in f1_stats.iterrows():
+            f.write(f"{contrast}: {row['mean']:.4f} ± {row['std']:.4f}\n")
+        f.write("\nSensitivity score per contrast (mean ± std)\n")
+        f.write(f"Global sensitivity score: {metrics_results['sensitivity_score'].mean():.4f} ± {metrics_results['sensitivity_score'].std():.4f}\n")
+        for contrast, row in sensitivity_stats.iterrows():
+            f.write(f"{contrast}: {row['mean']:.4f} ± {row['std']:.4f}\n")
     
     return None
 
