e9e6b8f8fa6ae5d46f44b3c456a5b3192c0b63ce
Pierre-Louis Benveniste <pierrelouis.benveniste03@gmail.com>
2025-02-11 23:09:12 -0500

    reformatted the figure

--- nnunet/plot_radiologist_evaluation.py
+++ nnunet/plot_radiologist_evaluation.py
@@ -78,8 +78,10 @@ def main():
     df = df.reset_index(drop=True)
 
     # Rename column type to Segmentations and values GT to Manual and pred to Predicted
-    df['type'] = df['type'].replace({'GT': 'Manual', 'pred': 'Predicted'})
+    df['type'] = df['type'].replace({'GT': 'Manual', 'pred': 'Auto'})
     df = df.rename(columns={'type': 'Segmentations'})
+    # Convert score to int
+    df['score'] = df['score'].astype(int)
 
     # color palette is green for manual and yellow for predicted
     sns.set_palette(sns.color_palette(['lime', 'yellow']))
@@ -88,26 +90,47 @@ def main():
 
     # Now we plot a violin plot of the scores
     plt.figure(figsize=(10, 7))
-    sns.violinplot(data=df, x="rater", y="score", hue="Segmentations", split=True, gap=.1, inner="quart")    
-    plt.xlabel('Rater')
+    # y axis goes from 1 to 5
+    plt.ylim(-0.5, 6.5)
+    # But don't display the 0 and the 6
+    plt.yticks(np.arange(1, 6, 1))
+    # Add y grid
+    plt.grid(axis='y', linestyle='--', alpha=0.4)
+    sns.violinplot(data=df, x="rater", y="score", hue="Segmentations", split=True, gap=.1, inner="quart")
+    # Don't display the x legend
+    plt.xlabel('')
+    # Display mean and median in the graph
     plt.ylabel('Likert scores')
+    # For rater 1 auto, display the mean with a red line but only on the part of the graph which is regards
+    plt.axhline(y=df[(df['rater'] == 'Rater 1') & (df['Segmentations'] == 'Auto')]['score'].mean(), color='red', linestyle='--', alpha=1, xmin=0.175, xmax=0.265)
+    # For rater 1 manual, display the mean with a red line but only on the part of the graph which is regards
+    plt.axhline(y=df[(df['rater'] == 'Rater 1') & (df['Segmentations'] == 'Manual')]['score'].mean(), color='red', linestyle='--', alpha=1, xmin=0.07, xmax=0.16)
+    # For rater 2 auto, display the mean with a red line but only on the part of the graph which is regards
+    plt.axhline(y=df[(df['rater'] == 'Rater 2') & (df['Segmentations'] == 'Auto')]['score'].mean(), color='red', linestyle='--', alpha=1, xmin=0.508, xmax=0.57)
+    # For rater 2 manual, display the mean with a red line but only on the part of the graph which is regards
+    plt.axhline(y=df[(df['rater'] == 'Rater 2') & (df['Segmentations'] == 'Manual')]['score'].mean(), color='red', linestyle='--', alpha=1, xmin=0.418, xmax=0.49)
+    # For rater 3 auto, display the mean with a red line but only on the part of the graph which is regards
+    plt.axhline(y=df[(df['rater'] == 'Rater 3') & (df['Segmentations'] == 'Auto')]['score'].mean(), color='red', linestyle='--', alpha=1, xmin=0.843, xmax=0.91)
+    # For rater 3 manual, display the mean with a red line but only on the part of the graph which is regards
+    plt.axhline(y=df[(df['rater'] == 'Rater 3') & (df['Segmentations'] == 'Manual')]['score'].mean(), color='red', linestyle='--', alpha=1, xmin=0.74, xmax=0.82)
+
     plt.savefig(path_out / 'GT_pred_scores.png')
 
     # Now for each rater we compute wilcoxon test between GT and pred
     ## For rater 1
     rater_1 = df[df['rater'] == 'Rater 1']
     rater_1['score'] = rater_1['score'].astype(int)
-    diff1 = rater_1[rater_1['Segmentations'] == 'Manual']['score'].values - rater_1[rater_1['Segmentations'] == 'Predicted']['score'].values
+    diff1 = rater_1[rater_1['Segmentations'] == 'Manual']['score'].values - rater_1[rater_1['Segmentations'] == 'Auto']['score'].values
     stat1, p1 = wilcoxon(diff1)
     ## For rater 2
     rater_2 = df[df['rater'] == 'Rater 2']
     rater_2['score'] = rater_2['score'].astype(int)
-    diff2 = rater_2[rater_2['Segmentations'] == 'Manual']['score'].values - rater_2[rater_2['Segmentations'] == 'Predicted']['score'].values
+    diff2 = rater_2[rater_2['Segmentations'] == 'Manual']['score'].values - rater_2[rater_2['Segmentations'] == 'Auto']['score'].values
     stat2, p2 = wilcoxon(diff2)
     ## For rater 3
     rater_3 = df[df['rater'] == 'Rater 3']
     rater_3['score'] = rater_3['score'].astype(int)
-    diff3 = rater_3[rater_3['Segmentations'] == 'Manual']['score'].values - rater_3[rater_3['Segmentations'] == 'Predicted']['score'].values
+    diff3 = rater_3[rater_3['Segmentations'] == 'Manual']['score'].values - rater_3[rater_3['Segmentations'] == 'Auto']['score'].values
     stat3, p3 = wilcoxon(diff3)
     ## Print the results
     print(f'Rater 1: Statistics={stat1}, p={p1}')
