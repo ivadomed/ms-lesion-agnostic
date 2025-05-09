f82374b53c96c6fcfa61e6175903de6980c3a154
Pierre-Louis Benveniste <pierrelouis.benveniste03@gmail.com>
2025-05-09 09:17:01 -0400

    remved sbatch head

--- scripts/continue_train_job3.sh
+++ scripts/continue_train_job3.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-#SBATCH --account=aip-jcohen
-#SBATCH --job-name=job3     # set a more descriptive job-name 
-#SBATCH --nodes=1
-#SBATCH --gpus-per-node=h100:4
-#SBATCH --cpus-per-task=24
-#SBATCH --mem=80G
-#SBATCH --time=1-00:00:00   # DD-HH:MM:SS
-#SBATCH --output=/home/p/plb/links/scratch/ms-lesion-agnostic/model_trainings/job3/%x_%A_v2.out
-#SBATCH --error=/home/p/plb/links/scratch/ms-lesion-agnostic/model_trainings/job3/%x_%A_v2.err
-#SBATCH --mail-user=pierrelouis.benveniste03@gmail.com     # whenever the job starts/fails/completes, an email will be sent 
-#SBATCH --mail-type=ALL
 
 job_name="job3"
 
