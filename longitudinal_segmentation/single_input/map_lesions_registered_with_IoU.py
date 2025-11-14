b2a1f9bc01fec9263e38b55be64cf465bff6e39f
Pierre-Louis Benveniste <pierrelouis.benveniste03@gmail.com>
2025-11-14 18:59:17 +0100

    first draft of lesion matching using IoU

--- longitudinal_segmentation/single_input/map_lesions_registered_with_IoU.py
+++ longitudinal_segmentation/single_input/map_lesions_registered_with_IoU.py
@@ -38,7 +38,7 @@ def parse_args():
     return parser.parse_args()
 
 
-def map_lesions_registered_with_IoU(input_image1, input_image2, output_folder):
+def map_lesions_registered_with_IoU(input_image1, input_image2, output_folder, IoU_threshold=0.05):
     """
     This function performs lesion mapping between two timepoints using registered images and lesion matching based on the center of mass of lesions.
 
@@ -86,27 +86,66 @@ def map_lesions_registered_with_IoU(input_image1, input_image2, output_folder):
     labeled_lesion_seg_2 = os.path.join(temp_folder, image_2_name.replace('.nii.gz', '_lesion-seg_labeled.nii.gz'))
     labeled_lesion_seg_2_reg = os.path.join(temp_folder, image_2_name.replace('.nii.gz', '_lesion-seg_registered_labeled.nii.gz'))
 
-    # Segment the spinal cord
-    segment_sc(input_image1, sc_seg_1)
-    segment_sc(input_image2, sc_seg_2)
-    # Segment the lesions
-    segment_lesions(input_image1, sc_seg_1, qc_folder, lesion_seg_1, test_time_aug=True)
-    segment_lesions(input_image2, sc_seg_2, qc_folder, lesion_seg_2, test_time_aug=True)
-    # Get the levels
-    get_levels(input_image1, levels_1)
-    get_levels(input_image2, levels_2)
+    # # Segment the spinal cord
+    # segment_sc(input_image1, sc_seg_1)
+    # segment_sc(input_image2, sc_seg_2)
+    # # Segment the lesions
+    # segment_lesions(input_image1, sc_seg_1, qc_folder, lesion_seg_1, test_time_aug=True)
+    # segment_lesions(input_image2, sc_seg_2, qc_folder, lesion_seg_2, test_time_aug=True)
+    # # Get the levels
+    # get_levels(input_image1, levels_1)
+    # get_levels(input_image2, levels_2)
 
     # In the context of this registration, we need to only have common levels between both timepoints
     keep_common_levels_only(levels_1, levels_2)
 
-    # Register image 2 to image 1
-    parameter = "step=0,type=label,dof=Tx_Ty_Tz:step=1,type=im,algo=dl"
-    assert os.system(f"sct_register_multimodal -i {input_image2} -d {input_image1} -param '{parameter}' -ilabel {levels_2} -dlabel {levels_1} -o {registered_image2_to_1} -owarp {warping_field_img2_to_1} -owarpinv {inv_warping_field_img2_to_1} -dseg {sc_seg_1} -qc {qc_folder} ") == 0, "Registration failed"
+    # # Register image 2 to image 1
+    # parameter = "step=0,type=label,dof=Tx_Ty_Tz:step=1,type=im,algo=dl"
+    # assert os.system(f"sct_register_multimodal -i {input_image2} -d {input_image1} -param '{parameter}' -ilabel {levels_2} -dlabel {levels_1} -o {registered_image2_to_1} -owarp {warping_field_img2_to_1} -owarpinv {inv_warping_field_img2_to_1} -dseg {sc_seg_1} -qc {qc_folder} ") == 0, "Registration failed"
 
-    # We warp the lesion segmentation of image 2 to image 1 space using a linear interpolation
-    assert os.system(f"sct_apply_transfo -i {lesion_seg_2} -d {input_image1} -w {warping_field_img2_to_1} -o {lesion_seg_2_reg} -x nn") == 0, "Failed to warp lesion segmentation of image 2"
+    # # We warp the lesion segmentation of image 2 to image 1 space using a linear interpolation
+    # assert os.system(f"sct_apply_transfo -i {lesion_seg_2} -d {input_image1} -w {warping_field_img2_to_1} -o {lesion_seg_2_reg} -x nn") == 0, "Failed to warp lesion segmentation of image 2"
     
-    
+    # We label the lesion segmentation files
+    label_lesion_seg(lesion_seg_1, labeled_lesion_seg_1)
+    label_lesion_seg(lesion_seg_2, labeled_lesion_seg_2)
+    label_lesion_seg(lesion_seg_2_reg, labeled_lesion_seg_2_reg)
+
+    # Now we perform lesion matching betweemn timepoint 1 and timepoint 2 registered to timepoint 1 based on IoU
+    ## If two lesions have IoU > threshold, they are considered the same lesion
+    img_lesion_1 = nib.load(labeled_lesion_seg_1)
+    data_lesion_1 = img_lesion_1.get_fdata()
+    img_lesion_2_reg = nib.load(labeled_lesion_seg_2_reg)
+    data_lesion_2_reg = img_lesion_2_reg.get_fdata()
+    n_lesions_1 = int(data_lesion_1.max())
+    n_lesions_2 = int(data_lesion_2_reg.max())
+    logger.info(f"Number of lesions at timepoint 1: {n_lesions_1}")
+    logger.info(f"Number of lesions at timepoint 2: {n_lesions_2}")
+    # For each lesion at timepoint 1, compute IoU with each lesion at timepoint 2
+    IoU_matrix = np.zeros((n_lesions_1, n_lesions_2))
+    for i in range(1, n_lesions_1 + 1):
+        lesion_1_mask = (data_lesion_1 == i)
+        for j in range(1, n_lesions_2 + 1):
+            lesion_2_mask = (data_lesion_2_reg == j)
+            intersection = np.logical_and(lesion_1_mask, lesion_2_mask).sum()
+            union = np.logical_or(lesion_1_mask, lesion_2_mask).sum()
+            if union > 0:
+                IoU = intersection / union
+            else:
+                IoU = 0
+            IoU_matrix[i-1, j-1] = IoU
+    # print the lesion IoU matrix
+    logger.info(f"IoU matrix between lesions at timepoint 1 and timepoint 2:\n{IoU_matrix}")
+    # Now we use the Hungarian algorithm to find the best matching between lesions based on IoU
+    cost_matrix = 1 - IoU_matrix  # We want to maximize IoU, so we minimize 1 - IoU
+    row_ind, col_ind = linear_sum_assignment(cost_matrix)
+    # Now we filter the matches based on the IoU threshold
+    lesion_id_mapping = {} # old lesion id at timepoint 2 to new lesion id at timepoint 1
+    for i, j in zip(row_ind, col_ind):
+        if IoU_matrix[i, j] >= IoU_threshold:
+            lesion_id_mapping[int(j + 1)] = int(i + 1)  # lesion ids are 1-indexed
+    logger.info(f"Lesion ID mapping from timepoint 2 to timepoint 1 based on IoU threshold of {IoU_threshold}: {lesion_id_mapping}")
+    ## ALSO NEED TO LOOK AT HOW TO MAP LESIONS IN THE OTHER DIRECTION ALSO
 
     return None
 
