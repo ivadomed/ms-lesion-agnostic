79257807dcaeeb670d6d151fd32f5b4ffd6430a6
Pierre-Louis Benveniste <pierrelouis.benveniste03@gmail.com>
2025-11-14 06:30:06 -0500

    added warping of lesion 2 to img1

--- longitudinal_segmentation/single_input/map_lesions_registered_with_CoM.py
+++ longitudinal_segmentation/single_input/map_lesions_registered_with_CoM.py
@@ -41,6 +41,7 @@ def parse_args():
 def keep_common_levels_only(levels_1, levels_2):
     """
     This function keeps only the common disc levels between two level segmentations.
+    I could have done this using 'sct_label_utils -remove-reference'
 
     Inputs:
         levels_1 : path to the levels segmentation at timepoint 1
@@ -82,8 +83,20 @@ def compute_lesion_CoM(lesion_seg):
     Outputs:
         lesion_CoM : list of CoM coordinates for each lesion
     """
+    # Load lesion segmentation
+    img_lesion = nib.load(lesion_seg)
+    data_lesion = img_lesion.get_fdata()
+    # Label connected components (lesions)
+    labeled_lesions, num_lesions = ndimage.label(data_lesion)
+    lesion_CoM = {}
+    for lesion_id in range(1, num_lesions + 1):
+        lesion_mask = (labeled_lesions == lesion_id)
+        if np.sum(lesion_mask) == 0:
+            continue
+        com = ndimage.center_of_mass(lesion_mask)
+        lesion_CoM[lesion_id] = com
 
-    return None
+    return lesion_CoM
 
 
 def map_lesions_registered_with_CoM(input_image1, input_image2, output_folder):
@@ -117,14 +130,18 @@ def map_lesions_registered_with_CoM(input_image1, input_image2, output_folder):
     # Initialize file names for lesions, sc and disc levels at both timepoints
     image_1_name = Path(input_image1).name
     lesion_seg_1 = os.path.join(temp_folder, image_1_name.replace('.nii.gz', '_lesion-seg.nii.gz'))
-    sc_seg_1 = os.path.join(output_folder, image_1_name.replace('.nii.gz', '_sc_seg.nii.gz'))
+    sc_seg_1 = os.path.join(temp_folder, image_1_name.replace('.nii.gz', '_sc_seg.nii.gz'))
     levels_1 = os.path.join(temp_folder, image_1_name.replace('.nii.gz', '_levels.nii.gz'))
     image_2_name = Path(input_image2).name
     lesion_seg_2 = os.path.join(temp_folder, image_2_name.replace('.nii.gz', '_lesion-seg.nii.gz'))
-    sc_seg_2 = os.path.join(output_folder, image_2_name.replace('.nii.gz', '_sc_seg.nii.gz'))
+    sc_seg_2 = os.path.join(temp_folder, image_2_name.replace('.nii.gz', '_sc_seg.nii.gz'))
     levels_2 = os.path.join(temp_folder, image_2_name.replace('.nii.gz', '_levels.nii.gz'))
     # Initialize file name for registered image 2
     registered_image2_to_1 = os.path.join(temp_folder, image_2_name.replace('.nii.gz', '_registered_to_' + image_1_name))
+    warping_field_img2_to_1 = os.path.join(temp_folder, image_2_name.replace('.nii.gz', '_warp_to_' + image_1_name))
+    inv_warping_field_img2_to_1 = os.path.join(temp_folder, image_2_name.replace('.nii.gz', '_inv_warp_to_' + image_1_name))
+    # Initialize file name for lesion segmentation of image 2 registered to image 1
+    lesion_seg_2_reg = os.path.join(temp_folder, image_2_name.replace('.nii.gz', '_registered.nii.gz'))
 
     # # Segment the spinal cord
     # segment_sc(input_image1, sc_seg_1)
@@ -139,17 +156,34 @@ def map_lesions_registered_with_CoM(input_image1, input_image2, output_folder):
     # In the context of this registration, we need to only have common levels between both timepoints
     keep_common_levels_only(levels_1, levels_2)
 
-    # Register image 2 to image 1
-    parameter = "step=0,type=label,dof=Tx_Ty_Tz:step=1,type=im,algo=dl"
-    assert os.system(f"sct_register_multimodal -i {input_image2} -d {input_image1} -param '{parameter}' -ilabel {levels_2} -dlabel {levels_1} -o {registered_image2_to_1} -dseg {sc_seg_1} -qc {qc_folder} ") == 0, "Registration failed"
-    
-    ###
-    ## I NEED TO SAVE THE WARPING FIELD TO BE ABLE TO MOVE BACK LABELED LESIONS
-    ####
+    # # Register image 2 to image 1
+    # parameter = "step=0,type=label,dof=Tx_Ty_Tz:step=1,type=im,algo=dl"
+    # assert os.system(f"sct_register_multimodal -i {input_image2} -d {input_image1} -param '{parameter}' -ilabel {levels_2} -dlabel {levels_1} -o {registered_image2_to_1} -owarp {warping_field_img2_to_1} -owarpinv {inv_warping_field_img2_to_1} -dseg {sc_seg_1} -qc {qc_folder} ") == 0, "Registration failed"
 
-    # Compute lesion Center-of-Mass (CoM) on the image 1 and the registered image 2
+    # We warp the lesion segmentation of image 2 to image 1 space using a linear interpolation
+    assert os.system(f"sct_apply_transfo -i {lesion_seg_2} -d {input_image1} -w {warping_field_img2_to_1} -o {lesion_seg_2_reg} -x linear") == 0, "Failed to warp lesion segmentation of image 2"
+    
+    # WE SHOULD FIRST CREATE LABELED LESION SEGS FOR ALL 3 SEGMENTATIONS (IMAGE 1, IMAGE 2, IMAGE 2 REGISTERED)
+
+    # Compute lesion Center-of-Mass (CoM) on the image 1, image 2, and the registered image 2
     lesion_1_CoM = compute_lesion_CoM(lesion_seg_1)
     lesion_2_CoM = compute_lesion_CoM(lesion_seg_2)
+    lesion_2_reg_CoM = compute_lesion_CoM(lesion_seg_2_reg)
+
+    # We perform lesion matching based on the CoM using the Hungarian algorithm
+    cost_matrix = np.zeros((len(lesion_1_CoM), len(lesion_2_reg_CoM)))
+    for i, (lesion1_id, com1) in enumerate(lesion_1_CoM.items()):
+        for j, (lesion2_id, com2) in enumerate(lesion_2_reg_CoM.items()):
+            cost_matrix[i, j] = np.linalg.norm(np.array(com1) - np.array(com2))
+    row_ind, col_ind = linear_sum_assignment(cost_matrix)
+    print("Lesion mapping (lesion ID at timepoint 1 -> lesion ID at timepoint 2):")
+    for i, j in zip(row_ind, col_ind):
+        lesion1_id = list(lesion_1_CoM.keys())[i]
+        lesion2_id = list(lesion_2_reg_CoM.keys())[j]
+        distance = cost_matrix[i, j]
+        print(f"Lesion {lesion1_id} -> Lesion {lesion2_id} (Distance: {distance:.2f} voxels)")
+    
+    # Save the
 
     
 
