290bd268f1d8869d9ea903b0461ecbe64d9d3a2a
Pierre-Louis Benveniste <pierrelouis.benveniste03@gmail.com>
2025-11-26 02:57:12 -0500

    added edss in evaluation

--- longitudinal_segmentation/single_input/run_plot_evaluation.py
+++ longitudinal_segmentation/single_input/run_plot_evaluation.py
@@ -5,6 +5,7 @@ Input:
     - input_msd_dataset: Path to the MSD dataset folder.
     - predictions_folder: Path to the folder containing model predictions.
     - output_folder: Path to the folder where evaluation results will be saved.
+    - sub-edss: Path to a csv file containing subject EDSS scores.
 Output:
     None
 
@@ -19,7 +20,7 @@ import json
 import pandas as pd
 import numpy as np
 from prettytable import PrettyTable
-
+from scipy import stats
 
 
 def parse_args():
@@ -28,10 +29,11 @@ def parse_args():
     parser.add_argument('-i', '--input-msd', type=str, required=True, help='Path to the input MSD dataset')
     parser.add_argument('-p', '--predictions-folder', type=str, required=True, help='Path to the folder containing model predictions')
     parser.add_argument('-o', '--output-folder', type=str, required=True, help='Path to the output folder where evaluation results will be stored')
+    parser.add_argument('-s', '--sub-edss', type=str, required=True, help='Path to a csv file containing subject EDSS scores')
     return parser.parse_args()
 
 
-def plot_evaluation_results(results, output_folder):
+def plot_evaluation_results(results, output_folder, sub_edss):
     """
     This function plots the evaluation results.
 
@@ -45,6 +47,23 @@ def plot_evaluation_results(results, output_folder):
     logger.add(os.path.join(output_folder, 'plot_evaluation.log'))
     # Build a pandas dataframe from the results
     df = pd.DataFrame(results).T
+
+    # Load the subjects EDSS scores
+    edss_df = pd.read_csv(sub_edss)
+    # Remove some columns in edss_df
+    edss_df = edss_df.drop(columns=['file', 'number', 'site', 'name', 'original_name' ])
+
+    # Merge the two dataframes on subject ID
+    df = df.merge(edss_df, left_index=True, right_on='sub')
+
+    # For values in EDSS columns, we convert them to float
+    for col in edss_df.columns:
+        if col != 'sub' and col != 'phenotye':
+            # Replace , by . in the column
+            df[col] = df[col].str.replace(',', '.')
+            # Convert the column to float
+            df[col] = df[col].astype(float)
+
     # Convert column lesion_mapping_TP, lesion_mapping_FP, lesion_mapping_FN to their average values and std and sum and count
     for columns in ['lesion_mapping_TP', 'lesion_mapping_FP', 'lesion_mapping_FN']:
         df[columns + '_mean'] = df[columns].apply(lambda x: np.mean(x))
@@ -53,7 +72,7 @@ def plot_evaluation_results(results, output_folder):
         df[columns + '_count'] = df[columns].apply(lambda x: len(x))
     # For all columns, we print the mean and std and the median and range
     for column in df.columns:
-        if column in ['lesion_mapping_TP', 'lesion_mapping_FP', 'lesion_mapping_FN']:
+        if column in ['lesion_mapping_TP', 'lesion_mapping_FP', 'lesion_mapping_FN', 'sub', 'phenotye']:
             continue
         logger.info(f" ----- Plotting column {column} ----- ")
         logger.info(f"Mean: {df[column].mean()}, Std: {df[column].std()}")
@@ -99,6 +118,40 @@ def plot_evaluation_results(results, output_folder):
     table_mapping.add_row([f"FN", f"{df['lesion_mapping_FN_sum'].sum()}", f"{df['lesion_mapping_FN_sum'].mean():.2f} ± {df['lesion_mapping_FN_sum'].std():.2f}", f"{df['lesion_mapping_FN_mean'].mean():.2f} ± {df['lesion_mapping_FN_mean'].std():.2f}"])
     logger.info("\n" + table_mapping.get_string())
 
+    # Finally for each subject with non-0 EDSS_diff, we look at the correlation between EDSS diff and volume change
+    df_nonzero_edss = df[df['EDSS diff'] != 0]
+    # We compute the difference in lesion volume GT and predicted
+    df_nonzero_edss['vol_gt_diff_mm3'] = df_nonzero_edss['vol_gt2_mm3'] - df_nonzero_edss['vol_gt1_mm3']
+    df_nonzero_edss['vol_pred_diff_mm3'] = df_nonzero_edss['vol_pred2_mm3'] - df_nonzero_edss['vol_pred1_mm3']
+    
+    # We want to compute the correlation coefficient on ground truth volumes
+    # Plot the scatter plot between vol_gt_diff_mm3 and EDSS diff
+    plt.figure()
+    plt.scatter(df_nonzero_edss['vol_gt_diff_mm3'], df_nonzero_edss['EDSS diff'])
+    plt.xlabel('Lesion Volume Change (mm3)')
+    plt.ylabel('EDSS Change')
+    plt.title('Lesion Volume Change vs EDSS Change')
+    plt.grid()
+    plt.savefig(os.path.join(output_folder, 'gt_lesion_volume_change_vs_EDSS_change.png'))
+    
+    res = stats.spearmanr(list(df_nonzero_edss['vol_gt_diff_mm3']), list(df_nonzero_edss['EDSS diff']))
+    logger.info(f"Spearman correlation coefficient (GT volume): {res}")
+    res = stats.pearsonr(list(df_nonzero_edss['vol_gt_diff_mm3']), list(df_nonzero_edss['EDSS diff']))
+    logger.info(f"Pearson correlation coefficient (GT volume): {res}")
+    # We also want to compute the correlation coefficient on predicted volumes
+    ## Plot the scatter plot between vol_pred_diff_mm3 and EDSS diff
+    plt.figure()
+    plt.scatter(df_nonzero_edss['vol_pred_diff_mm3'], df_nonzero_edss['EDSS diff'])
+    plt.xlabel('Predicted Lesion Volume Change (mm3)')
+    plt.ylabel('EDSS Change')
+    plt.title('Predicted Lesion Volume Change vs EDSS Change')
+    plt.grid()
+    plt.savefig(os.path.join(output_folder, 'predicted_lesion_volume_change_vs_EDSS_change.png'))
+    res = stats.spearmanr(list(df_nonzero_edss['vol_pred_diff_mm3']), list(df_nonzero_edss['EDSS diff']))
+    logger.info(f"Spearman correlation coefficient (predicted volume): {res}")
+    res = stats.pearsonr(list(df_nonzero_edss['vol_pred_diff_mm3']), list(df_nonzero_edss['EDSS diff']))
+    logger.info(f"Pearson correlation coefficient (predicted volume): {res}")
+
     return None
 
 
@@ -107,8 +160,13 @@ if __name__ == "__main__":
     input_msd_dataset = args.input_msd
     predictions_folder = args.predictions_folder
     output_folder = args.output_folder
+    sub_edss = args.sub_edss
 
     # Run the evaluation
     results = evaluate_lesion_mapping(input_msd_dataset, predictions_folder, output_folder)
 
-    plot_evaluation_results(results, output_folder)
+    # results_path = "~/net/longitudinal_ms/20251121_lesion_matching_reg_com_results_debug/overall_results.json"
+    # with open(os.path.expanduser(results_path), 'r') as f:
+    #     results = json.load(f)
+
+    plot_evaluation_results(results, output_folder, sub_edss)
